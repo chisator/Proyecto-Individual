2f1248f33b04f22bba0145b986686807fa931c87
chisator <gianfrancosacchi@hotmail.com.ar>
2023-01-04 19:22:30 -0300

    subiendo el proyecto al repositorio

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path=require('path')
+const port=procss.env.PORT || 3000
+
+app.listen(port,()=>console.log('Servidor corriendo en puerto '+ port))
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html')))
+app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
